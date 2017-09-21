8ddefa087a1751f8d5df9047bdf6f1e2a091aa9e
abudel <andrea.budel@gmail.com>
2017-09-21 15:10:22 +0200

    android

--- android/src/main/java/com/reactlibrary/RNAdyenCseModule.java
+++ android/src/main/java/com/reactlibrary/RNAdyenCseModule.java
@@ -5,6 +5,12 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
+import adyen.com.adyencse.encrypter.exception.EncrypterException;
+
+import adyen.com.adyencse.pojo.Card;
+
+import java.util.Date;
 
 public class RNAdyenCSEModule extends ReactContextBaseJavaModule {
 
@@ -18,5 +24,27 @@ public class RNAdyenCSEModule extends ReactContextBaseJavaModule {
   @Override
   public String getName() {
     return "RNAdyenCSE";
+  }
+
+  @ReactMethod
+  public void encrypt(String holderName, String number, String cvc, String expiryMonth, String expiryYear,
+      String publicKey, Promise promise) {
+
+    Card card = new Card();
+    card.setCardHolderName(holderName);
+    card.setCvc(cvc);
+    card.setExpiryMonth(expiryMonth);
+    card.setExpiryYear(expiryYear);
+    card.setGenerationTime(new Date());
+    card.setNumber(number);
+
+    try {
+      //	Encrypt card data
+      String encryptedCard = card.serialize(publicKey);
+
+      promise.resolve(encryptedCard);
+    } catch (EncrypterException e) {
+      promise.reject("ADYEN_CSE_ERROR", e);
+    }
   }
 }
